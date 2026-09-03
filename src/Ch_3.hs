913482d42dff577f57587e26821bcec96ba06da7
Eitan Barylko <eitan@barylko.com>
2026-09-02 21:30:31 -0700

    Added a test

--- src/Ch_3.hs
+++ src/Ch_3.hs
@@ -6,17 +6,21 @@ where
 
 import Data.List(intercalate)
 import Control.Lens
+import Data.Char(isSpace)
 
 data User = User { _firstName :: String
                  , _lastName :: String
                  , _email :: String}
-          deriving (Show)
+          deriving (Show, Eq)
 makeLenses ''User
 
 fullName :: Lens' User String
 --fullName = view firstName user (++) " " ++ view lastName user
 fullName = lens getFullName (error "x") where
   getFullName = (intercalate " ")  <$>  (toList <$> view firstName  <*> view lastName)
+  setFullName user newFullName = set firstName (extractFirstName newFullName) user & set firstName (extractLastName newFullName)
+  extractFirstName = takeWhile (not . isSpace)
+  extractLastName = dropWhile (not . isSpace)
   toList x y = [x, y]
 
 someFunc :: IO ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,3 +10,7 @@ main = hspec $ do
   describe "viewing the full name of a user" $ do
     it "is the concatenation of their first and last name" $ do
       view fullName defaultUser `shouldBe` "John Cena"
+
+  describe "Setting a new full name of a user of the form firstName remainingPart" $ do
+    it "sets the new first name as firstName and the last name as remainingPart" $ do
+      set fullName "Doctor of Thuganomics" defaultUser `shouldBe` defaultUser
