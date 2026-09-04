8dae134058b9ad306b1a38a58831bdf84481467f
Eitan Barylko <62489101+ebarylko@users.noreply.github.com>
2026-09-04 11:12:49 -0700

    Chapter five (#1)

---
+++ src/Ch_5.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Ch_5 (Gate(..),
+            Army(..),
+            Kingdom(..),
+            lens1,
+            lens2,
+            lens3,
+            lens4)
+where
+
+import Control.Lens
+import Control.Arrow((>>>))
+import Data.Char(toUpper)
+
+
+data Gate = Gate { _open :: Bool
+                 , _oilTemp :: Float}
+          deriving (Show, Eq)
+makeLenses ''Gate
+
+data Army = Army { _archers :: Int ,
+                   _knights :: Int} deriving (Show, Eq)
+makeLenses ''Army
+
+data Kingdom = Kingdom { _name :: String,
+                         _army :: Army ,
+                         _gate :: Gate} deriving (Show, Eq)
+makeLenses ''Kingdom
+
+lens1 :: Kingdom -> Kingdom
+lens1 = name %~ (++ ": a perfect place") >>> army . knights .~ 42 >>> gate . open .~ False
+
+
+lens2 :: Kingdom -> Kingdom
+lens2 = name %~ (++ "instein") >>> army . knights .~ 26 >>> gate . oilTemp .~ 100 >>> army . archers .~ 17
+
+lens3 :: Kingdom -> (String, Kingdom)
+lens3 = name %~ (++ ": Home") >>> gate . oilTemp .~ 5 >>> name <<%~ (++ " of the talking Donkeys")
+
+lens4 :: ((Bool, String), Float) -> ((Bool, String), Float)
+lens4 =
+  _1 . _2 <>~ " - the worst"
+  >>> _2 -~ 15
+  >>> _2 //~ 2
+  >>> _1 . _2 %~ map toUpper
+  >>> _1 . _1 .~ False
---
+++ test/Ch5Spec.hs
@@ -0,0 +1,42 @@
+module Ch5Spec (spec) where
+import Ch_5(Gate(..),
+            Army(..),
+            Kingdom(..),
+            lens1,
+            lens2,
+            lens3,
+            lens4)
+
+import Test.Hspec
+
+duloc :: Kingdom
+duloc = Kingdom { _name= "Duloc" ,
+                  _army = Army { _archers= 22 , _knights= 14} ,
+                  _gate= Gate { _open= True , _oilTemp= 10.0}}
+
+spec :: Spec
+
+spec = do
+  describe "Chapter five exercises" $ do
+    it "Applying the first lens has the intended effect" $ do
+      let expected = Kingdom { _name= "Duloc: a perfect place" ,
+                               _army = Army { _archers= 22 , _knights= 42} ,
+                               _gate= Gate { _open= False , _oilTemp= 10.0}}
+      lens1 duloc `shouldBe` expected
+
+    it "Applying the second lens has the intended effect" $ do
+      let expected = Kingdom { _name= "Dulocinstein" ,
+                               _army = Army { _archers= 17 , _knights= 26} ,
+                               _gate= Gate { _open= True , _oilTemp= 100.0}}
+      lens2 duloc `shouldBe` expected
+
+    it "Applying the third lens has the intended effect" $ do
+      let expected = ( "Duloc: Home" ,
+                       Kingdom { _name= "Duloc: Home of the talking Donkeys"
+                               , _army = Army { _archers= 22 , _knights= 14}
+                               , _gate= Gate { _open= True , _oilTemp= 5.0}})
+      lens3 duloc `shouldBe` expected
+
+    it "Applying the fourth lens has the intended effect" $ do
+      let expected = ((False, "DUDLEY - THE WORST"),20.0)
+      lens4 ((True, "Dudley"), 55.0) `shouldBe` expected
